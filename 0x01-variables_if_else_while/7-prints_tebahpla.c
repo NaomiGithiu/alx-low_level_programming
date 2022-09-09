18c20f3e1825433ebb9395c40ea73becb69187e6
Naomi Githiu <githiunaomi@gmail.com>
2022-09-09 15:59:07 -0700

    prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-prints_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>                                                                                                                                                               
+/**                                                                                                                                                                              
+ * main fuction prints the alphabets in lowercase                                                                                                                               
+ *                                                                                                                                                                               
+ * description: using the main function                                                                                                                                         
+ * this program prints the alphabets                                                                                                                                             
+ * return: 0                                                                                                                                                                     
+ */                                                                                                                                                                              
+int main(void)                                                                                                                                                                   
+{                                                                                                                                                                                
+char ch;                                                                                                                                                                         
+for (ch = 'z'; ch <= 'a'; ch--)                                                                                                                                                  
+{                                                                                                                                                                                
+        putchar(ch);                                                                                                                                                             
+}                                                                                                                                                                                
+putchar('\n');                                                                                                                                                                   
+return (0);                                                                                                                                                                      
+}                             
