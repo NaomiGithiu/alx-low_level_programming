4eba2d101256b13b3676c6ca1ffda0cc12143704
Naomi Githiu <githiunaomi@gmail.com>
2022-09-09 15:20:50 -0700

    all alphabets excluding e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,14 +3,14 @@
 *main - prints if the number is positive, negative or zero
 *description: using the main function
 *this program prints "alphabets in lowercase excluding e and q"
-*return 0
+*return "(0);"
 */
 int main(0)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch != 'e' && ch !='q')
+if (ch != 'e' && ch != 'q')
 {
 	putchar(ch);
 }
