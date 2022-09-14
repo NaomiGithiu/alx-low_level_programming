169389575ab937024f516b19ddd5643098675729
Naomi Githiu <githiunaomi@gmail.com>
2022-09-14 03:03:45 -0700

    prints 10 times the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
+*print_alphabet - prints abc..
 *
 *
-* print_alphabet - prints the alphabets followed by a new line
 *Return: 0
 */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
