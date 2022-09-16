5484059e929b2879be1cf5670fa9e177fbce3bf4
Naomi Githiu <githiunaomi@gmail.com>
2022-09-16 02:53:05 -0700

    comments

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - prints an infinite loop
+*\\o/\n:its a comments
+*Return: 0
+*/
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/**
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
