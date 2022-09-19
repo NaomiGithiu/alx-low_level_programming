74c2523d3650facc3f5e4c9a54bdf8059871bdfd
Naomi Githiu <githiunaomi@gmail.com>
2022-09-19 09:10:51 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_rev - prints a string in reverse
+*
+*@s: is the string
+*Return: void
+*/
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		_putchar(*(s + len));
+	_putchar(10);
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*rev_string - reverses a string
+*
+*@s: string
+*Return: void
+*/
+void rev_string(char *s)
+{
+
