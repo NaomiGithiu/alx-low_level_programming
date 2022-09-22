5588825262ae0fc8e8c4ab4ed926e6d78e1425fe
Naomi Githiu <githiunaomi@gmail.com>
2022-09-22 01:34:58 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
-	whle (dest[index++])
+	while (dest[index++])
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
